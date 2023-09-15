15e427a753cc6ad9abfe35de7001d9c7d3f0a0d6
unknown <pokhrelshibam@gmail.com>
2023-09-14 19:48:08 -0500

    modified main

--- doubleLinearSearch/main.py
+++ doubleLinearSearch/main.py
@@ -3,4 +3,4 @@ import numpy as np
 array = np.array([10, 50, 16, 1, 9, 15, 16, 20, 16, 2, 5])
 searchValue = 16
 searchResult = linearSearch(array, searchValue)
-print(searchResult) # output should be {5,6}
+print(searchResult)
